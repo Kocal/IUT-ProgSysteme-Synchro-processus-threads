c2d14ffc9453a6198c19c8c8ca13a423e4553ac0
ALLIAUME Hugo <kocal@live.fr>
2015-11-17 15:00:14 +0100

    Partie 1.2 terminée

--- 1.2-heritage-table-descripteurs-entrelacement-processus.cpp
+++ 1.2-heritage-table-descripteurs-entrelacement-processus.cpp
@@ -1,1 +1,48 @@
+#include <iostream>
 
+#include <cstdlib>
+#include <cstdio>
+
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+using std::string;
+using std::cout;
+using std::endl;
+
+int main(void) {
+
+    string ping = "Ping-";
+    string pong = "Pong\n";
+
+    int fichier = open("match.txt",  O_CREAT | O_TRUNC | O_WRONLY, 0755);
+    pid_t pid = NULL;
+
+    if(fichier == -1) {
+        perror("Erreur match.txt");
+        exit(EXIT_FAILURE);
+    }
+
+    for(int i = 0; i < 3; i++) {
+        pid = fork();
+
+        if( pid == -1 ) {
+            perror("Erreur fork");
+            exit(EXIT_FAILURE);
+        } else if( pid > 0 ) { // Pere
+            write(fichier, ping.c_str(), ping.length());
+        } else { // Fils
+            write(fichier, pong.c_str(), pong.length());
+        }
+    }
+
+    close(fichier);
+    cout << "Fin de " << getpid() << endl;
+    return EXIT_SUCCESS;
+}
--- Synchronisation-processus-threads.pro
+++ Synchronisation-processus-threads.pro
@@ -5,7 +5,8 @@ CONFIG -= qt
 CONFIG += c++11
 
 SOURCES += \
-    1.1-synchronisation-processus-tubes.cpp
+    #1.1-synchronisation-processus-tubes.cpp \
+    1.2-heritage-table-descripteurs-entrelacement-processus.cpp
 
 HEADERS +=
 
